a4a5c931651937222685169cd6417275626eac80
暮秋pro <oncwnuDcKAa9aHtUN1_rnIGw84kY@git.weixin.qq.com>
2023-10-24 15:14:00 +0800

    配置了库存管理路由,树形菜单,目录创建及首页样式冲突

--- crm-frontend/src/stores/user.js
+++ crm-frontend/src/stores/user.js
@@ -132,7 +132,43 @@ let testMenus = [
     id: 4,
     text: '库存管理',
     icon: 'IconHouse',
-    children: []
+    children: [
+      {
+        id: 2,
+        text: '库存清单',
+        href: '/un-21',
+        icon: 'IconHouse',
+        pid: 4
+      },
+      {
+        id: 3,
+        text: '入库单',
+        href: '/un-21',
+        icon: 'IconHouse',
+        pid: 4
+      },
+      {
+        id: 4,
+        text: '入库明细',
+        href: '/inventory/WarehousingDetails',
+        icon: 'IconHouse',
+        pid: 4
+      },
+      {
+        id: 5,
+        text: '出库单',
+        href: '/un-21',
+        icon: 'IconHouse',
+        pid: 4
+      },
+      {
+        id: 6,
+        text: '出库明细',
+        href: '/un-21',
+        icon: 'IconHouse',
+        pid: 4
+      }
+    ]
   }
 ]
 
---
+++ crm-frontend/src/views/inventoryManagement/inventoryManagement.vue
@@ -0,0 +1,23 @@
+<script setup>
+// 这个界面是库存管理路由出口页面
+// 需要配置一下树形菜单,配置形势可以参考'@/views/sample/SampleView.vue'
+// 哪位大佬有空可以搞一下
+</script>
+<template>
+  <div class="view">
+    <!-- <h3 class="meau">库存管理树形菜单</h3> -->
+    <router-view></router-view>
+  </div>
+</template>
+<style>
+.view {
+  display: flex;
+}
+/* .meau {
+  font-weight: 800;
+  padding: 20px;
+  border: 1px solid #333;
+  border-radius: 4px;
+  margin: 0 20px 0 0;
+} */
+</style>
