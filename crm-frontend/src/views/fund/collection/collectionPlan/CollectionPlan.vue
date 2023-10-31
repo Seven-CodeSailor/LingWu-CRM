ad72bd83514fa6e000c0e3462dbba1107b8b7aee
sayoriqwq <2531600563@qq.com>
2023-10-31 20:58:31 +0800

    Collection部分

--- crm-frontend/src/views/fund/collection/collectionPlan/CollectionPlan.vue
+++ crm-frontend/src/views/fund/collection/collectionPlan/CollectionPlan.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>回款计划</div>
+  <BaseDataList
+    :tableData="$store.sendData.tableData"
+    :tableColumnAttribute="$store.sendData.tableColumnAttribute"
+    :useHeader="$store.sendData.useHeader"
+    :useOperateColumn="$store.sendData.useOperateColumn"
+    :usePagination="$store.sendData.usePagination"
+  ></BaseDataList>
 </template>
 
-<script setup></script>
+<script setup>
+import { useCollectionPlan } from '@/stores/fund/collection/collectionPlan.js'
+import { onMounted } from 'vue'
+import BaseDataList from '@/components/DataList/BaseDataList.vue'
+const $store = useCollectionPlan()
+
+onMounted(() => {
+  //页面初始化，加载数据
+  $store.getCollectionPlanList(1, 10)
+})
+</script>
 
 <style lang="scss" scoped></style>
