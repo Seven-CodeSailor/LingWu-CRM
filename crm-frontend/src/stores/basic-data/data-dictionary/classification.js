c16a9218a869fed65f8a1fb41bf40a1ba3a14a70
BINGWU <HuJiaCheng2003@163.com>
2023-11-03 13:12:27 +0800

    提交

--- crm-frontend/src/stores/basic-data/data-dictionary/classification.js
+++ crm-frontend/src/stores/basic-data/data-dictionary/classification.js
@@ -1,12 +1,9 @@
 /*
- * @Author: BINGWU HuJiaCheng2003@163.com
  * @Date: 2023-10-31 15:54:40
  * @LastEditors: BINGWU HuJiaCheng2003@163.com
- * @LastEditTime: 2023-11-01 16:57:45
+ * @LastEditTime: 2023-11-03 13:10:36
  * @FilePath: \crm-frontend\src\stores\basic-data\data-dictionary\classification.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import {
