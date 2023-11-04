6613ea57a65ff2454e59c4675798071c68f03882
setti5 <2283356040@qq.com>
2023-11-04 21:15:33 +0800

    index, https加假token

--- crm-frontend/src/apis/personal-homapage/message.js
+++ crm-frontend/src/apis/personal-homapage/message.js
@@ -1,27 +1,105 @@
 /*
  * @Author: pipazi 2283356040@qq.com
  * @Date: 2023-11-03 12:56:03
- * @LastEditors: pipazi 2283356040@qq.com
- * @LastEditTime: 2023-11-03 19:56:53
+ * @LastEditors: setti5 2283356040@qq.com
+ * @LastEditTime: 2023-11-03 23:21:06
  * @FilePath: \zero-one-crmsys\crm-frontend\src\apis\personal-homapage\message.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 import Request from '@/apis/request.js'
 
-//请求接口的模板
-// export const test = async (data, success, fail) => {
-//     await Request.requestJson(
-//       Request.GET,
-//       'https://mockapi.eolink.com/69jekXFeb89500c132462100d3745b0046ecd1264eeb224/test',
-//       {
+// 个人首页——消息通知接口
+/**
+ * @description: 删除消息（单条）
+ * @param {*} params  //请求参数: ids
+ * @param success  //成功回调
+ * @param fail  //失败回调
+ * @return {Promise}
+ * {
+        "code": 10000,
+        "data": true,
+        "message": "提示消息内容"
+    }
+ */
+export const deleteMessage = async (params) => {
+    return await Request.requestJson(
+      Request.DELETE,
+      'https://mockapi.eolink.com/KnVGhupeb89500c132462100d3745b0046ecd1264eeb224/dictmanage/get-dict',
+      {
+        params
+      } 
+    )
+    .then((res) => {
+        success(res)
+      })
+      .catch((err) => {
+        fail(err)
+      })
+  }
+  
+/**
+ * @description: 标记消息已读（批量操作）
+ * @param {*} params //请求参数: ids
+ * @return {*}
+ * {
+        "code": 10000,
+        "data": true,
+        "message": "提示消息内容"
+    }
+    */
+export const markMessageAsRead= (params) => {
+    return Request.requestJson(
+      Request.POST,
+      'https://mockapi.eolink.com/KnVGhupeb89500c132462100d3745b0046ecd1264eeb224/add-dictclassify',
+      params
+    )
+  }
 
-//       }
-//     )
-//       .then((response) => {
-//         success(response)
-//       })
-//       .catch((error) => {
-//         fail(error)
-//       })
-//   }
+/**
+ * @description: 发布消息 
+ * @param {*} params //请求参数: {msgTitle(消息主题), msgType(消息类型), ownerUserId(所属用户编号), remindTime(提醒时间), urlId(URL编号), urlType(URL类型) }
+ * @return {*}
+ * {
+        "code": 10000,
+        "data": true,
+        "message": "提示消息内容"
+    }
+ */
+export const releaseMessage= (params) => {
+    return Request.requestJson(
+      Request.POST,
+      'https://mockapi.eolink.com/KnVGhupeb89500c132462100d3745b0046ecd1264eeb224/add-dictclassify',
+      params
+    )
+  }
+
+/**
+ * @description: 根据分页条件获取消息列表
+ * @param {Object} params // 请求参数: {flag(消息是否被处理	), keyWord(关键字), msgType(消息类型), pageIndex(查询页码), pageSize(查询条数)}
+ * @return {Promise}
+ */
+export const getMessageList = async (params) => {
+    return await Request.requestJson(
+      Request.GET,
+      'https://mockapi.eolink.com/KnVGhupeb89500c132462100d3745b0046ecd1264eeb224/dictmanage/get-dict',
+      {
+        params
+      }
+    )
+  }
+
+  /**
+ * @description: 查看消息详情
+ * @param {Object} params // 请求参数: {id}
+ * @return {Promise}
+ */
+export const getMessageDetails = async (params) => {
+    return await Request.requestJson(
+      Request.GET,
+      'https://mockapi.eolink.com/KnVGhupeb89500c132462100d3745b0046ecd1264eeb224/dictmanage/get-dict',
+      {
+        params
+      }
+    )
+  }
--- crm-frontend/src/apis/personal-homapage/notice.js
+++ crm-frontend/src/apis/personal-homapage/notice.js
@@ -2,37 +2,36 @@
  * @Author: pipazi 2283356040@qq.com
  * @Date: 2023-11-01 21:29:40
  * @LastEditors: setti5 2283356040@qq.com
- * @LastEditTime: 2023-11-03 21:04:16
+ * @LastEditTime: 2023-11-03 22:37:11
  * @FilePath: \zero-one-crmsys\crm-frontend\src\apis\personal-homapage\notice.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 import Request from '@/apis/request.js'
 
+// 个人首页 —— 公告通知接口
 /**
- * @description: 个人首页——公告通知——发布公告
+ * @description: 发布公告
  * @param {*} params  //请求参数:{content(内容), createUserId(创建人), status(查看状态), title(标题), ownerDeptId(接收部门id), ownerUserId(接收人id), ...,}
- * @param {*} success
- * @param {*} fail
- * @return {*}
+ * @param {*} success  //成功回调
+ * @param {*} fail  //失败回调
+ * @return {Promise}
  * {
       "code": 10000,
       "data": true,
       "message": "提示消息内容"
     }
  */
-export const postNotice = async (params, success, fail) => {
+export const releaseNotice = async (params, success, fail) => {
   await Request.requestJson(
     Request.POST,
     'https://mock.apifox.cn/m1/3500676-0-default/getdata',
     {}
   )
     .then((response) => {
-      console.log(1)
       success(response)
     })
     .catch((error) => {
-      console.log(2)
       fail(error)
     })
 }
@@ -42,7 +41,7 @@ export const postNotice = async (params, success, fail) => {
  * @param {*} params  //请求参数: {id(id)}
  * @param {*} success
  * @param {*} fail
- * @return {*}
+ * @return {Promise}
  * {
       "code": 10000,
       "data": {
@@ -73,10 +72,10 @@ export const getNoticeDetails = async (params,success, fail) => {
 
 /**
  * @description: 获取系统公告
- * @param {*} params  //请求参数:
+ * @param {*} params  //请求参数:{content(公告内容), pageIndex(查询页码), pageSize(查询条数), title(公告标题), ...,}
  * @param {*} success
  * @param {*} fail
- * @return {*}
+ * @return {Promise}
  * {
       "code": 10000,
       "data": {
@@ -91,10 +90,58 @@ export const getNoticeDetails = async (params,success, fail) => {
       "message": "提示消息内容"
     }
  */
-    export const getPagedict = async (params,success, fail) => {
+    export const getSystemNotice = async (params,success, fail) => {
       await Request.requestJson(
         Request.GET,
         'https://mockapi.eolink.com/KnVGhupeb89500c132462100d3745b0046ecd1264eeb224/dictmanage/get-Pagedict'
+      )
+        .then((response) => {
+          success(response)
+        })
+        .catch((error) => {
+          fail(error)
+        })
+    }
+
+/**
+ * @description: 删除公告（可批量删除）
+ * @param {Object} params  //请求参数: ids
+ * @param {*} success
+ * @param {*} fail
+ * @return {Promise}
+ * {
+      "code": 10000,
+      "data": true,
+      "message": "提示消息内容"
+    }
+ */
+export const deleteNotice = async (params) => {
+  return await Request.requestJson(
+    Request.POST,
+    'https://mockapi.eolink.com/KnVGhupeb89500c132462100d3745b0046ecd1264eeb224/dictmanage/get-dict',
+    {
+      params
+    }
+  )
+}
+
+/**
+ * @description: 标记已读公告（可批量标记）
+ * @param {*} params  //请求参数:{content(内容), createUserId(创建人), status(查看状态), title(标题), ownerDeptId(接收部门id), ownerUserId(接收人id), ...,}
+ * @param {*} success
+ * @param {*} fail
+ * @return {Promise}
+ * {
+      "code": 10000,
+      "data": true,
+      "message": "提示消息内容"
+    }
+ */
+    export const markNoticeAsRead = async (params, success, fail) => {
+      await Request.requestJson(
+        Request.POST,
+        'https://mock.apifox.cn/m1/3500676-0-default/getdata',
+        {}
       )
         .then((response) => {
           success(response)
