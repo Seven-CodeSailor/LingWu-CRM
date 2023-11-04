8bd989f90d6a7dac9bdfe178b41d08335cc6e8b2
aaabinbinbin <1685052085@qq.com>
2023-11-04 23:13:12 +0800

    修复f1

---
+++ crm-frontend/src/stores/basic-data/components/SelectTreeSearch.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Author: BINGWU
+ * @Date: 2023-11-04 15:38:43
+ * @LastEditors: BINGWU HuJiaCheng2003@163.com
+ * @LastEditTime: 2023-11-04 15:41:37
+ * @FilePath: \crm-frontend\src\stores\basic-data\components\SelectTreeSearch.vue
+ * @Describe: 
+ * @Mark: ૮(˶ᵔ ᵕ ᵔ˶)ა
+-->
+<template>
+  <div class="SelectTreeSearch"></div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
