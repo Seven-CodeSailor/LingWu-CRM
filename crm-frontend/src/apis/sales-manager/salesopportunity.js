509a07dec1a595078186b1e1acda6bb325a071c2
seven <1473008948@qq.com>
2023-11-05 21:03:10 +0800

    代码维护修改

---
+++ crm-frontend/src/apis/sales-manager/salesopportunity.js
@@ -0,0 +1,20 @@
+import Request from '../request'
+
+/**
+ * @description: 获取销售机会分页列表  参数字段配置的路径:  crm-frontend\src\stores\salesmanager\SalesContract.js
+ * @param {*} data
+ *
+ */
+
+export const querySalesOpportunity = (data) => {
+  return Request.requestJson(
+    Request.GET,
+    //TODO
+    'https://mockapi.eolink.com/fhdz9TQ7632232d76db4f62838f5c8793d22b77760ead7b/​opportunities​/sales-oppo-list',
+    {
+      params: {
+        ...data
+      }
+    }
+  )
+}
