c6414b15450f4156c15416e1b69d79dc25709bfb
暮秋pro <oncwnuDcKAa9aHtUN1_rnIGw84kY@git.weixin.qq.com>
2023-11-09 00:55:37 +0800

    最后的开发,再见了-暮秋

--- crm-frontend/src/stores/organizationStructure/postManagement.js
+++ crm-frontend/src/stores/organizationStructure/postManagement.js
@@ -1,8 +1,8 @@
 /*
  * @Author: 暮秋pro oncwnuDcKAa9aHtUN1_rnIGw84kY@git.weixin.qq.com
  * @Date: 2023-11-04 23:26:00
- * @LastEditors: sayoriqwq 2531600563@qq.com
- * @LastEditTime: 2023-11-05 21:11:14
+ * @LastEditors: 暮秋pro oncwnuDcKAa9aHtUN1_rnIGw84kY@git.weixin.qq.com
+ * @LastEditTime: 2023-11-08 23:13:29
  * @FilePath: \zero-one-crmsys\crm-frontend\src\stores\organizationStructure\postManagement.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -24,9 +24,24 @@ const usePostManageStore = defineStore('post', () => {
   const setUserPostList = (dataList) => {
     usePostList.value = dataList
   }
+  // 定义岗位名称结构树
+  const postTreeMeau = ref()
+  const setPostTreeMeau = (dataList) => {
+    postTreeMeau.value = dataList
+  }
+  // 页面表格的数据
+  const postListTable = ref()
+  const setPostListTable = (dataList) => {
+    postListTable.value = dataList
+  }
+
   return {
     usePostList,
-    setUserPostList
+    setUserPostList,
+    postTreeMeau,
+    setPostTreeMeau,
+    postListTable,
+    setPostListTable
   }
 })
 export default usePostManageStore
