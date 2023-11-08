0d3c32cc6da2df0c2104ca6d9bbdd3753b6930b0
sayoriqwq <2531600563@qq.com>
2023-11-08 21:27:44 +0800

    暂时不用menu

--- crm-frontend/src/router/index.js
+++ crm-frontend/src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: sayoriqwq 2531600563@qq.com
  * @Date: 2023-10-25 22:24:51
  * @LastEditors: sayoriqwq 2531600563@qq.com
- * @LastEditTime: 2023-11-08 13:12:12
+ * @LastEditTime: 2023-11-08 21:25:33
  * @FilePath: \zero-one-crmsys\crm-frontend\src\router\index.js
  * @Description:
  *
@@ -83,7 +83,7 @@ router.beforeEach(async function (to, from, next) {
       // 加载用户信息
       await store.loadUser()
       // 加载菜单资源
-      await store.loadMenus()
+      // await store.loadMenus()
       // 设置加载完毕
       store.setLoaded(true)
     }
