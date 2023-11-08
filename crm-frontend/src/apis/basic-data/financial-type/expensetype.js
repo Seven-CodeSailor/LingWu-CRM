d1bd62a9b734eb9b013b0e7dd0b9d8822ccfb4ed
BINGWU <HuJiaCheng2003@163.com>
2023-11-08 21:10:25 +0800

    最后一次-bingwu

--- crm-frontend/src/apis/basic-data/financial-type/expensetype.js
+++ crm-frontend/src/apis/basic-data/financial-type/expensetype.js
@@ -2,10 +2,11 @@
  * @Author: BINGWU
  * @Date: 2023-11-07 22:08:08
  * @LastEditors: BINGWU HuJiaCheng2003@163.com
- * @LastEditTime: 2023-11-07 22:32:52
+ * @LastEditTime: 2023-11-08 21:10:05
  * @FilePath: \crm-frontend\src\apis\basic-data\financial-type\expensetype.js
  * @Describe:费用开支模块的api
  * @Mark: ૮(˶ᵔ ᵕ ᵔ˶)ა
  */
 import Request from '@/apis/request.js'
 const baseUrl = '/cpp1-apiv1'
+// 干不动了
